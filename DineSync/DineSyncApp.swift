8a094839223e82c80a1aad034fcdfeb61771b8b6
Pravar Chauhan <chauhanpravar7@gmail.com>
2025-03-30 16:45:48 -0700

    Initial Commit

---
+++ DineSync/DineSyncApp.swift
@@ -0,0 +1,17 @@
+//
+//  DineSyncApp.swift
+//  DineSync
+//
+//  Created by Pravar Chauhan on 3/30/25.
+//
+
+import SwiftUI
+
+@main
+struct DineSyncApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ DineSyncTests/DineSyncTests.swift
@@ -0,0 +1,17 @@
+//
+//  DineSyncTests.swift
+//  DineSyncTests
+//
+//  Created by Pravar Chauhan on 3/30/25.
+//
+
+import Testing
+@testable import DineSync
+
+struct DineSyncTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
